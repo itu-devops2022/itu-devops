f07e0838896a85d9b1d7482c55812e17b55410bc
muki <muki@itu.dk>
2022-03-08 13:51:32 +0100

    one more test passing for simulator api

--- test/minitwit_elixir_web/controllers/api_controller_test.exs
+++ test/minitwit_elixir_web/controllers/api_controller_test.exs
@@ -103,6 +103,76 @@ defmodule MinitwitElixirWeb.ApiControllerTest do
   end
 
   test "a unfollows b", %{conn: conn} do
+    #register users
+    data_a = [
+      username: "a",
+      email: "a@a.a",
+      pwd: "a",
+      latest: "1"
+    ]
+
+    data_b = [
+      username: "b",
+      email: "b@b.b",
+      pwd: "b",
+      latest: "5"
+    ]
+
+    data_c = [
+      username: "c",
+      email: "c@c.c",
+      pwd: "c",
+      latest: "6"
+    ]
+    conn_test = conn |>
+      post("/api/register", data_a) |>
+      post("/api/register", data_b) |>
+      post("/api/register", data_c)
+
+    #a follows b and c
+    data_follow_ab = [
+      follow: "b",
+      latest: "7"
+    ]
+
+    data_follow_ac = [
+      follow: "c",
+      latest: "8"
+    ]
+
+    data_confirm = [
+      no: "20",
+      latest: "9"
+    ]
+
+    conn_test2 = conn_test |>
+      post("/api/fllws/a", data_follow_ab) |>
+      post("/api/fllws/a", data_follow_ac)
+
+    #a unfollows b
+    data_unfollow_ab = [
+      unfollow: "b",
+      latest: "10"
+    ]
+
+    data_confirm_2 = [
+      no: "20",
+      latest: "11"
+    ]
+
+    conn_test3 = conn_test2 |>
+      post("/api/fllws/a", data_unfollow_ab)
+    assert text_response(conn_test3, 204)
+
+    conn_test4 = conn_test3 |>
+      get("/api/fllws/a", data_confirm_2)
+    refute Enum.at(json_response(conn_test4, 200)["follows"], 0) == "b"
+    assert Enum.at(json_response(conn_test4, 200)["follows"], 0) == "c"
+
+    # verify that latest was updated
+    conn_test5 = conn_test4 |>
+      get("/api/latest")
+    assert json_response(conn_test5, 200) == %{"latest" => 11}
 
   end
 
