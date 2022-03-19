6698e5d2956e549cdec445a63954fa714a85d590
muki <muki@itu.dk>
2022-03-19 17:13:30 +0100

    - persisting metrics in storage - need a way to update the actual telemetry

--- lib/minitwit_elixir_web/controllers/api/telemetry_controller.ex
+++ lib/minitwit_elixir_web/controllers/api/telemetry_controller.ex
@@ -4,13 +4,24 @@ defmodule MinitwitElixirWeb.Api.TelemetryController do
 
   def export_metric(conn, _params) do
     #IO.inspect("exporting metrics")
-    conn |>
-      put_status(200) |>
-      text("exporting metrics")
+    case File.read("metrics") do
+      {:ok, body} ->
+        conn |>
+          put_status(200) |>
+          json(%{metrics: body})
+      {:error, reason} ->
+        conn |>
+          put_status(503) |>
+          json(%{error_msg: reason})
+    end
+
   end
 
   def import_metric(conn, params) do
     IO.inspect("importing metrics: #{params["metrics"]}")
+    {:ok, file} = File.open("metrics", [:write])
+    IO.binwrite(file, params["metrics"])
+    File.close(file)
     conn |>
       put_status(200) |>
       text("importing metrics")
